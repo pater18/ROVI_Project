440b8c502ae59d865697a1d856a3dfff2ebc19ec
emilbak5 <emilbak5@gmail.com>
2021-12-13 17:19:55 +0100

    s

---
+++ src/add_noice.h
@@ -0,0 +1,85 @@
+#include <pcl/point_cloud.h>
+#include <pcl/common/random.h>
+#include <pcl/common/time.h>
+#include <pcl/common/common.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/spin_image.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/registration/correspondence_rejection_sample_consensus.h>
+#include <pcl/registration/transformation_estimation_svd.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <iostream>
+#include <pcl/point_types.h>
+
+
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/convolution_3d.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/mls.h>
+#include <pcl/filters/passthrough.h>
+
+
+using namespace std;
+using namespace pcl;
+using namespace pcl::common;
+using namespace pcl::io;
+using namespace pcl::registration;
+using namespace pcl::search;
+using namespace pcl::visualization;
+using namespace Eigen;
+
+
+typedef PointNormal PointT;
+
+void spatialFilter( pcl::PointCloud<PointT>::Ptr input_cloud, pcl::PointCloud<PointT>::Ptr &output_cloud )
+{
+	
+  // Create the filtering object
+  pcl::PassThrough<PointT> pass;
+  pass.setInputCloud (input_cloud);
+  pass.setFilterFieldName ("z");
+  pass.setFilterLimits (-1.5, -1);
+  pass.filter (*output_cloud);
+
+  pass.setFilterFieldName("y");
+  pass.setFilterLimits(-0.371274, 0.197482);
+  pass.filter (*output_cloud);
+
+}
+
+void addNoice(const std::string object_name, const std::string scene_name )
+{
+
+    // Load
+    pcl::PointCloud<PointT>::Ptr scene(new pcl::PointCloud<PointT>);
+    pcl::io::loadPCDFile<PointT> (scene_name, *scene);
+
+    PointCloud<PointT>::Ptr scene_filtered (new PointCloud<PointT> ());
+    scene_filtered->points.resize (scene->points.size ());
+
+    spatialFilter(scene, scene);
+
+    {
+        PCLVisualizer v("Before global alignment");
+        v.addPointCloud<PointT>(scene, PointCloudColorHandlerCustom<PointT>(scene, 255, 0, 0), "scene");
+        v.spin();
+    }
+
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.01,0.02);
+
+    for (size_t point_i = 0; point_i < scene->points.size(); ++point_i)
+    {
+        scene_filtered->points[point_i].x = scene->points[point_i].x + static_cast<float>(distribution(generator));
+        scene_filtered->points[point_i].y = scene->points[point_i].y + static_cast<float>(distribution(generator));
+        scene_filtered->points[point_i].z = scene->points[point_i].z + static_cast<float>(distribution(generator));
+    }
+
+    {
+        PCLVisualizer v("Before global alignment");
+        v.addPointCloud<PointT>(scene, PointCloudColorHandlerCustom<PointT>(scene, 255, 0, 0), "scene");
+        v.spin();
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,9 @@
 #include <rw/loaders/image/ImageLoader.hpp>
 #include <rw/loaders/path/PathLoader.hpp>
 #include <rw/trajectory/LinearInterpolator.hpp>
-#include "interpolation.h"
-#include "reachability.h"
-#include "denseStereo.h"
-#include "poseEstimate.h"
-#include "poseEstimate2.h"
-#include <pcl/point_types.h>
-#include "errorOfPose.h"
 
+#include <pcl/point_types.h>
+#include "add_noice.h"
 
 
 
@@ -45,39 +40,8 @@ using namespace Eigen;
 
 int main(int argc, char** argv)
 {
-	// Bottle poses
-    std::vector<Vector3D<> > vectors;
-    vectors.push_back(Vector3D<>(-0.30, 0.40, 0.21));
-    vectors.push_back(Vector3D<>(-0.30, 0.50, 0.21));
-    vectors.push_back(Vector3D<>(-0.10, 0.40, 0.21));
-    vectors.push_back(Vector3D<>(-0.10, 0.50, 0.21));
-    vectors.push_back(Vector3D<>( 0.10, 0.40, 0.21));
-    vectors.push_back(Vector3D<>( 0.10, 0.50, 0.21));
-    vectors.push_back(Vector3D<>( 0.30, 0.40, 0.21));
-    vectors.push_back(Vector3D<>( 0.30, 0.50, 0.21));
-
-    RPY<> R1 = RPY<>(-1.571, 0, 1.571);
-
-    std::vector<rw::math::Transform3D<> > bottle_transformations;
-    rw::math::Transform3D<> temp = Transform3D<>(vectors[0], R1.toRotation3D());
-
-    for (size_t i = 0; i < vectors.size(); i++)
-    {
-        temp = Transform3D<>(vectors[i], R1.toRotation3D());
-        bottle_transformations.push_back(temp);
-    }
-
-	//std::vector<rw::math::Transform3D<> > poses_actual = getPoseWithDenseStereo();
-	//Matrix4f pose = poseEstimatePCLAnders("bottle.ply", "scene_clouds/cloud_scene3.pcd");
-	poseEstimatePCL("bottle2.ply", "scene_clouds/cloud_scene3.pcd");
-
-    Matrix4f pose;    
-    for (size_t i = 0; i < bottle_transformations.size(); i++)
-    {
-        calcErrorOnPose(bottle_transformations[i], pose);
-
-    }
 	
+	addNoice("bottle2.ply", "scene_clouds/cloud_scene3.pcd");
 
 	return 0;
 }
